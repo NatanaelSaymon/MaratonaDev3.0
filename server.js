bce1ae27b01e7029c8e5c1e9deafb6b1b79af8d9
Natanael Saymon <nsaymonsouza@gmail.com>
2020-02-24 18:45:25 -0300

    estrutura de repetição para mostrar os doadores

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+//Para iniciar o server no browser
+// npm start
+
+//Para finalizar o server
+//ctrl + c 
+
+
 //Configurando o servidor
 const express = require("express")
 const server = express()
@@ -7,16 +14,58 @@ const server = express()
 server.use(express.static('public'))
 
 
+//Habilitar o body do formulario
+server.use(express.urlencoded({ extended:true }))
+
+
 //Configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
-    express: server
+    express: server,
+    noCache: true,
 })
 
 
+//Lista de doadores: vetor ou array
+const donors = [
+    {
+        name: "Suellen Souza",
+        blood: "AB+"
+    },
+    {
+        name: "Natanael",
+        blood: "O+"
+    },
+    {
+        name: "Cacau Souza",
+        blood: "B+"
+    },
+    {
+        name: "Natsu Souza",
+        blood: "A-"
+    }
+]
+
+
 //Configurar a apresentação da pagina
-server.get("/", function(req, res){
-    return res.render("index.html")
+server.get("/", function(req, res){ //get pegar dados
+    return res.render("index.html", { donors })
+})
+
+server.post("/", function(req, res){ //post guardar dados
+    //pegar dados do formularios usando o req
+    const name  = req.body.name
+    const email  = req.body.email
+    const blood  = req.body.blood
+
+    //colonaod valores dentro de um array
+    donors.push({
+        name: name,
+        blood: blood,
+    })
+
+    //Resposta de redirecionamento
+    return res.redirect("/")
 })
 
 
