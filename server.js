2d53636027e6d58bba73f361154e4bbb6bbb37dd
Natanael Saymon <nsaymonsouza@gmail.com>
2020-02-25 12:51:32 -0300

    aplicação finalizada

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //Para iniciar o server no browser
 // npm start
+//http://127.0.0.1:3000/
 
 //Para finalizar o server
 //ctrl + c 
+
+//Banco de dados doe
+//log: postgres
+//senha: 22142203
 
 
 //Configurando o servidor
@@ -18,6 +23,17 @@ server.use(express.static('public'))
 server.use(express.urlencoded({ extended:true }))
 
 
+//confugirar a conexao com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '22142203',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
+
 //Configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -26,31 +42,17 @@ nunjucks.configure("./", {
 })
 
 
-//Lista de doadores: vetor ou array
-const donors = [
-    {
-        name: "Suellen Souza",
-        blood: "AB+"
-    },
-    {
-        name: "Natanael",
-        blood: "O+"
-    },
-    {
-        name: "Cacau Souza",
-        blood: "B+"
-    },
-    {
-        name: "Natsu Souza",
-        blood: "A-"
-    }
-]
-
-
 //Configurar a apresentação da pagina
 server.get("/", function(req, res){ //get pegar dados
-    return res.render("index.html", { donors })
+    db.query(`SELECT * FROM donors`, function(err, result){
+        if(err) return res.send("erro no banco de dados.")
+
+        const donors = result.rows;
+        return res.render("index.html", { donors })
+    })
+
 })
+
 
 server.post("/", function(req, res){ //post guardar dados
     //pegar dados do formularios usando o req
@@ -58,14 +60,25 @@ server.post("/", function(req, res){ //post guardar dados
     const email  = req.body.email
     const blood  = req.body.blood
 
-    //colonaod valores dentro de um array
-    donors.push({
-        name: name,
-        blood: blood,
+    //regra de negocio para não enviar dados em branco
+    if(name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são obrigatórios.")
+    }
+
+    //colocando valores dentro do banco de dados
+    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err){
+        //fluxo de erro
+        if(err) return res.send("erro no banco de dados.")
+
+        //Fluxo ideal. Resposta de redirecionamento
+        return res.redirect("/")
+
     })
 
-    //Resposta de redirecionamento
-    return res.redirect("/")
 })
 
 
